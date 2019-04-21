51496a01f9b543385c6f67d1374360ee8ad92a80
carloshlemos <chenriquelemos@gmail.com>
2019-04-21 18:11:22 -0300

    improving BaseResourceService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, throwError } from 'rxjs';
@@ -6,96 +6,17 @@ import { map, catchError, flatMap } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
+import { extend } from 'webdriver-js-extender';
+import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class EntryService {
+export class EntryService extends BaseResourceService<Entry> {
 
-  private apiPath = 'api/entries';
-
-  constructor(
-    private http: HttpClient,
-    private categoryService: CategoryService
-  ) { }
-
-  getAll(): Observable<Entry[]> {
-    return this.http.get(this.apiPath).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntries)
-    );
+  constructor(protected injector: Injector) {
+    super('api/entries', injector);
   }
 
-  getById(id: number): Observable<Entry> {
-    const url = `${this.apiPath}/${id}`;
-
-    return this.http.get(url).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
-    );
-  }
-
-  create(entry: Entry): Observable<Entry> {
-
-    //Necessário devido ao angular-in-memory-api
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-
-        return this.http.post(this.apiPath, entry).pipe(
-          catchError(this.handleError),
-          map(this.jsonDataToEntry)
-        );
-
-      })
-    );
-  }
-
-  update(entry: Entry): Observable<Entry> {
-    const url = `${this.apiPath}/${entry.id}`;
-
-    //Necessário devido ao angular-in-memory-api
-    return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => {
-        entry.category = category;
-
-        return this.http.put(this.apiPath, entry).pipe(
-          catchError(this.handleError),
-          map(() => entry)
-        );
-      })
-    );
-  }
-
-  delete(id: number): Observable<any> {
-    const url = `${this.apiPath}/${id}`;
-
-    return this.http.delete(url).pipe(
-      catchError(this.handleError),
-      map(() => null)
-    );
-  }
-
-  private jsonDataToEntry(jsonData: any): Entry {
-    return Object.assign(new Entry(), jsonData);
-  }
-
-  private jsonDataToEntries(jsonData: any[]): Entry[] {
-    const entries: Entry[] = [];
-
-    jsonData.forEach(element => {
-      const entry = Object.assign(new Entry(), element);
-      entries.push(entry);
-    });
-
-    return entries;
-  }
-
-  private handleError(error: any): Observable<any> {
-    console.log('ERRO NA REQUISIÇÃO => ', error);
-
-    return throwError(error);
-
-  }
 }
