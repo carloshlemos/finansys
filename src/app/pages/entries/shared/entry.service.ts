3194a4e98859c94408f7b9f6e8d63fffe6fe9842
carloshlemos <chenriquelemos@gmail.com>
2019-04-22 22:43:35 -0300

    improving BaseResourceService && EntryService && CategoryService
    ;

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -6,8 +6,9 @@ import { map, catchError, flatMap } from 'rxjs/operators';
 
 import { Entry } from './entry.model';
 import { CategoryService } from '../../categories/shared/category.service';
-import { extend } from 'webdriver-js-extender';
 import { BaseResourceService } from 'src/app/shared/services/base-resource.service';
+
+import * as moment from 'moment';
 
 
 @Injectable({
@@ -15,8 +16,47 @@ import { BaseResourceService } from 'src/app/shared/services/base-resource.servi
 })
 export class EntryService extends BaseResourceService<Entry> {
 
-  constructor(protected injector: Injector) {
-    super('api/entries', injector);
+  constructor(protected injector: Injector, private categoryService: CategoryService) {
+    super('api/entries', injector, Entry.fromJson);
   }
+
+  create(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
+  }
+
+  update(entry: Entry): Observable<Entry> {
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+  }
+
+  getByMonthAndYear(month: number, year: number): Observable<Entry[]> {
+    return this.getAll().pipe(
+      map(entries => this.filterByMonthAndYear(entries, month, year))
+    );
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap(category => {
+        entry.category = category;
+        return sendFn(entry)
+      }),
+      catchError(this.handleError)
+    );
+  }
+
+  private filterByMonthAndYear(entries: Entry[], month: number, year: number) {
+    return entries.filter(entry => {
+      const entryDate = moment(entry.date, 'DD/MM/YYYY');
+      const monthMatches = entryDate.month() + 1 == month;
+      const yearMatches = entryDate.year() == year;
+
+      if (monthMatches && yearMatches) {
+        return entry;
+      }
+    });
+  }
+
+
+
 
 }
